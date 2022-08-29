349ef5a7e7347a15720628f912b68d5d50fe330b
Andreydenisov7788 <asasa88881974@mail.ru>
2022-08-29 23:53:21 +0300

    2 переменные 1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        byte mouse = 1;
+        System.out.println(" Значение переменной с типом byte  равно " +  mouse );
+        short bull = 2;
+        System.out.println(" Значение переменной с типом bull  равно " + bull );
+        int tiger = 3;
+        System.out.println(" Значение переменной с типом int  равно " + tiger );
+        long rabbit = 4L;
+        System.out.println(" Значение переменной с типом long  равно " + rabbit);
+        float dragonMass = 5.5f;
+        System.out.println(" Значение переменной с типом float  равно " + dragonMass);
+        double snakeMass = 6.6;
+        System.out.println(" Значение переменной с типом double  равно " + snakeMass);
+        char minus = 45;
+        System.out.println(" Значение переменной с типом char  равно " + minus);
+        boolean tigerIsAdult = tiger > 4;
+        System.out.println(" Значение переменной с типом boolean  равно " + tigerIsAdult);
+
+
+
+    }
+}
