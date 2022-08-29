6e0915afa60ebbb6e0cec57f058dd009c6e071c6
Andreydenisov7788 <asasa88881974@mail.ru>
2022-08-30 00:44:24 +0300

    2переменныe 3

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        byte mouse = 1;
-        System.out.println(" Значение переменной с типом byte  равно " +  mouse );
-        short bull = 2;
-        System.out.println(" Значение переменной с типом bull  равно " + bull );
-        int tiger = 3;
-        System.out.println(" Значение переменной с типом int  равно " + tiger );
-        long rabbit = 4L;
-        System.out.println(" Значение переменной с типом long  равно " + rabbit);
-        float dragonMass = 5.5f;
-        System.out.println(" Значение переменной с типом float  равно " + dragonMass);
-        double snakeMass = 6.6;
-        System.out.println(" Значение переменной с типом double  равно " + snakeMass);
-        char minus = 45;
-        System.out.println(" Значение переменной с типом char  равно " + minus);
-        boolean tigerIsAdult = tiger > 4;
-        System.out.println(" Значение переменной с типом boolean  равно " + tigerIsAdult);
+       int totalSheets = 480;
+       int ludmilaStudents = 23;
+       int annaStudents = 27;
+       int katiaStudents = 30;
+       int totalStudents =  ludmilaStudents + annaStudents + katiaStudents;
+       System.out.println(totalStudents);
+       int sheetsPerPerson = totalSheets / totalStudents;
+        System.out.println(" На каждого ученика расчитано " + sheetsPerPerson + " листов ");
+
 
 
 
