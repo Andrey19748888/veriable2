9082530e5a5a89f3b2dbddf3ad643c98fc0e4ac1
Andreydenisov7788 <asasa88881974@mail.ru>
2022-08-30 01:06:27 +0300

    2переменныe 4

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        byte mouse = 1;
-        System.out.println(" Значение переменной с типом byte  равно " +  mouse );
-        short bull = 2;
-        System.out.println(" Значение переменной с типом bull  равно " + bull );
-        int tiger = 3;
-        System.out.println(" Значение переменной с типом int  равно " + tiger );
-        long rabbit = 4L;
-        System.out.println(" Значение переменной с типом long  равно " + rabbit);
-        float dragonMass = 5.5f;
-        System.out.println(" Значение переменной с типом float  равно " + dragonMass);
-        double snakeMass = 6.6;
-        System.out.println(" Значение переменной с типом double  равно " + snakeMass);
-        char minus = 45;
-        System.out.println(" Значение переменной с типом char  равно " + minus);
-        boolean tigerIsAdult = tiger > 4;
-        System.out.println(" Значение переменной с типом boolean  равно " + tigerIsAdult);
-
+        byte doesPerMinute = 16/2;
+        short makesInDay = 1440 * 8;
+        int doesItInThreeDays = 11520 * 3;
+        int doesInMonth = doesItInThreeDays * 30;
+        System.out.println(" За одини сутки машина произвела бутылок " + makesInDay + " штук");
+        System.out.println(" За три дня  машина произвела бутылок " + doesItInThreeDays + " штук ");
+        System.out.println(" За один месяц машина произвела бутылок " + doesInMonth + " штук ");
 
 
     }
